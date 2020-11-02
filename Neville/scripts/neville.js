14e25a0f04c556f3817c04b06686591ce7281fb3
Mikołaj Stempin <m.stempin99@gmail.com>
2020-11-02 20:08:44 +0100

    Formula display

--- Neville/scripts/neville.js
+++ Neville/scripts/neville.js
@@ -1,10 +1,10 @@
-function neville(X, Y, input){
+function nevilleResult(X, Y, input){
+
 	let len = X.length;
 	let result = Array.from(Array(len), () => new Array(len));
 
 	for (i = 0; i < len; i++)
 		result[i][0] = parseInt(Y[i]);
-
 	for (i = 1; i < len; i++) {
 		for (j = 1; j <= i; j++) {
 			result[i][j] = ((input - X[i - j])*(result[i][j - 1])
@@ -13,6 +13,23 @@ function neville(X, Y, input){
 	}
 
 	return result[result.length-1][result.length-1];
+}
+
+function nevilleFormula(X, Y){
+
+	let len = X.length;
+	let result = Array.from(Array(len), () => new Array(len));
+
+	for (i = 0; i < len; i++)
+		result[i][0] = Y[i];
+	for (i = 1; i < len; i++) {
+		for (j = 1; j <= i; j++) {
+			result[i][j] = math.simplify("((" + "x" + "-" + X[i - j].toString() + ")*(" + result[i][j - 1].toString()+")" +
+				"- (" + "x" + "-" + X[i].toString() + ")*(" + result[i - 1][j - 1].toString() + "))/(" + X[i].toString() + "-" +  X[i - j].toString() + ")");
+		}
+	}
+
+	return math.rationalize(result[result.length-1][result.length-1]);
 }
 
 function getResult(){
@@ -24,8 +41,15 @@ function getResult(){
 	if(!valid(X,Y, input))
 		return;
 
-	let resEl = document.getElementById('result');
-	resEl.innerHTML = "Wynik:   P(" + input + ") = " + neville(X,Y, input);
+	let formulaOutput  = document.getElementById('formula');
+	formulaOutput.innerHTML = '\`' + nevilleFormula(X,Y) + '\`';
+
+	let resultOutput  = document.getElementById('result');
+	resultOutput.innerHTML = "\`Wynik:   P(" + input + ") = " + nevilleResult(X,Y, input) + "\`";
+
+	MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 
 	drawGraph(X, Y);
 }
+
+
